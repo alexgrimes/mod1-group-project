ab57d1462898cc1bdcb09cc170a86f4527e6efd2
github username <github email address>
2020-05-27 15:56:08 -0500

    5/27 4pm

--- lib/Customer.rb
+++ lib/Customer.rb
@@ -2,12 +2,14 @@ class Customer < ActiveRecord::Base
   has_many :reviews 
   has_many :restaurants, through: :reviews
 
-  # def change
-  #   create_table :customers do |t|
-  #     t.string :name
-  #     t.string :email
-  #   end
-  # end 
+  def create_review(args = {} )
+    Review.create do |m|
+      m.rating = args[:rating]
+      m.restaurant = args[:restaurant]
+      m.save
+    end
+  end 
+
 end
 
 # A user can have many reviews 
--- lib/Review.rb
+++ lib/Review.rb
@@ -1,4 +1,48 @@
 class Review < ActiveRecord::Base
   belongs_to :restaurant
   belongs_to :customer
+
+  def create_review(args = {} )
+    Review.create do |r|
+      r.rating = args[:rating]
+      r.restaurant = args[:restaurant]
+      r.save
+    end
+  end 
+
+  # def new_review(attributes)
+  #   review = Review.new
+  #   review.rating = :rating
+  #   review.restaurant = :restaurant
+  #   review.save 
+  # end
+
+  # def can_be_instantiated_and_then_saved
+  #   movie = Movie.new
+  #   movie.title = "This is a title."
+  #   movie.save
+  # end
+
+  def self.find_review_by_restaurant(restaurant_name)
+    #look through table of reviews 
+    #match restaurant name to user input(argument?)
+    #return all reviews that match that instance name
+    self.find_by :restaurant == restaurant_name
+    puts :restaurant
+  end 
+
+  def self.find_max_review
+    Review.maximum(:rating)
+  end
+
+  def self.ratings_sum
+    self.sum(:rating)
+  end 
+
+  def self.update_review
+    review = Review.find_by(:customer, "Sid")
+    review.update(:rating, 6)
+    review.save
+  end 
+
 end 
